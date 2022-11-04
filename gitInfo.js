1bdb461f9e7bb52b948212210c4d146c0226687a
Eric Drake <puturosule@gmail.com>
2022-11-04 13:55:34 -0600

    Finished gitinfo.js

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,7 @@
 */
 
 //CODE HERE
+let gitDefinition = `Git is a version control system that tracks changes made to files in a project. It allows multiple developers to work on the same code base without overwriting each other's work.`;
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +19,7 @@
 */
 
 //CODE HERE
+let gitHubDefinition = `GitHub is a website for storing and sharing git repos in the cloud.`;
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -26,6 +28,7 @@
 */
 
 //CODE HERE
+let gitInitDefinition = `git init creates a new local repo. It creates the .git folder in the directory in which git init is run.`;
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -34,6 +37,7 @@
 */
 
 //CODE HERE
+let gitCloneDefinition = `git clone creates a local copy of a remote git repo.`;
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -42,6 +46,7 @@
 */
 
 //CODE HERE
+let gitStatusDefinition = `git status is a command that shows which branch git is currently checked out. It also shows a list of modified or staged files.`;
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -53,6 +58,8 @@
 */
 
 //CODE HERE
+let gitAddDefinition = `git add stages any files specified in the command.`;
+let gitAddCode = `git add .`;
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -64,7 +71,8 @@
 */
 
 //CODE HERE
-
+let gitCommitDefinition = `git commit takes a snapshot of the current state of the staged files`;
+let gitCommitCode = `git commit -m 'initial commit'`;
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +81,4 @@
 */
 
 //CODE HERE
+let gitPushDefinition = `git push takes the commited changes and uploads them to the specified branch of the remote repo`;
