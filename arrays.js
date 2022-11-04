0f6a9bfc90be80e92a9f72dc617014ff2b8b2948
Eric Drake <puturosule@gmail.com>
2022-11-04 13:43:16 -0600

    Finished arrays.js

--- arrays.js
+++ arrays.js
@@ -5,10 +5,16 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+//To make a full copy of faveColors, I only provided the slice method with the first index of the array as the first parameter
+//Without a second parameter, the method assumes that I want to include the remainder of the array as well
+const colorCopy = faveColors.slice(0);
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+//Added purple to the end of the colorCopy array
+colorCopy.push('purple');
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,7 +25,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+//created a new array of only the middle three numbers of the numbers array
+const middleNums = numbers.splice(1,3);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -31,6 +38,20 @@ const numbers = [1, 2, 3, 4, 5]
   to the answers array. 
 */
 
-const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
-
 // CODE HERE
+const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352];
+//created an empty array to hold 'big' or 'small' string values
+const answers = [];
+for(let i = 0; i < bigOrSmallArray.length; i++)
+{
+  //checked if the value in the bigOrSmallArray was greater than 100
+  if(bigOrSmallArray[i] > 100)
+  {
+    answers.push('big');
+  }
+  //checked if the value in the bigOrSmallArray was less than or equal to 100
+  else if(bigOrSmallArray[i] <= 100)
+  {
+    answers.push('small');
+  }
+}
